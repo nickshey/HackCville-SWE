edfb582b55a03999f4c65c072c4bfad87d65cab7
Taylor Rohrich <taylorrohrich@gmail.com>
2017-06-01 15:28:33 -0400

    added Firebase

--- app/src/components/Blog.js
+++ app/src/components/Blog.js
@@ -1,110 +1,148 @@
 import React, { Component } from 'react';
-
+import * as firebase from "firebase";
+var config = {
+    apiKey: "AIzaSyCyWUV-HmVRAXUWd5ENVn6VLK87fBJaxR4",
+    authDomain: "contacts-cda6f.firebaseapp.com",
+    databaseURL: "https://contacts-cda6f.firebaseio.com/",
+    projectId: "contacts-cda6f",
+    storageBucket: "contacts-cda6f.appspot.com",
+    messagingSenderId: "468700621018"
+  };
+firebase.initializeApp(config);
+var readData = firebase.database().ref('events/');
 var index = 0;
 class Blog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {temporary_thing: '', temporary_date: '', temporary_time: '', temporary_location: '', thing: '', date: '', time: '', location: '', events:[]};
-    this.handleThingChange = this.handleThingChange.bind(this);
-    this.handleDateChange = this.handleDateChange.bind(this);
-    this.handleTimeChange = this.handleTimeChange.bind(this);
-    this.handleLocationChange = this.handleLocationChange.bind(this);
-    this.handleThingEdit = this.handleThingEdit.bind(this);
-    this.handleDateEdit = this.handleDateEdit.bind(this);
-    this.handleTimeEdit = this.handleTimeEdit.bind(this);
-    this.handleLocationEdit = this.handleLocationEdit.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
+    
   }
 
-  handleThingChange(event) {
+  handleThingChange = event => {
     this.setState({...this.state, thing: event.target.value});
   }
-    handleDateChange(event) {
+    handleDateChange = event => {
     this.setState({...this.state, date: event.target.value});
   }
-    handleTimeChange(event) {
+    handleTimeChange = event => {
     this.setState({...this.state, time: event.target.value});
   }
-    handleLocationChange(event) {
+    handleLocationChange=event=> {
     this.setState({...this.state, location: event.target.value});
     }
-  handleThingEdit(event) {
+  handleThingEdit=event=> {
     this.setState({...this.state, temporary_thing: event.target.value});
   }
-    handleDateEdit(event) {
+    handleDateEdit=event=> {
     this.setState({...this.state, temporary_date: event.target.value});
   }
-    handleTimeEdit(event) {
+    handleTimeEdit=event=> {
     this.setState({...this.state, temporary_time: event.target.value});
   }
-    handleLocationEdit(event) {
+    handleLocationEdit=event=> {
     this.setState({...this.state, temporary_location: event.target.value})
     }
  
-  handleSubmit(event) {
+  handleSubmit=event=> {
     var array1=this.state.events,
     updatedEvents = array1.concat([{index: index, thing: this.state.thing, date: this.state.date, time: this.state.time, location: this.state.location }]);
     index++;
-    this.setState({
-      ...this.state,
-      events: updatedEvents
-    })
-  
+    this.writeUserData(updatedEvents)
     event.preventDefault();
   }
-  handleEditSubmit(anEvent) {
+  handleEditSubmit=anEvent=> {
     const updatedEvents = this.state.events.slice(0);
     updatedEvents[anEvent.index].editing = false;
     updatedEvents[anEvent.index].thing = this.state.temporary_thing + " "
     updatedEvents[anEvent.index].date = this.state.temporary_date + " "
     updatedEvents[anEvent.index].time = this.state.temporary_time + " "
     updatedEvents[anEvent.index].location = this.state.temporary_location + " "
+    /*
     this.setState({
       ...this.state,
       events: updatedEvents
     })
+    */
+    this.writeUserData(updatedEvents)
   }
 
-  handleEdit(anEvent) {
+  handleEdit=anEvent=> {
      const updatedEvents = this.state.events.slice(0);
      updatedEvents[anEvent.index].editing = true;
+     /*
      this.setState({
       ...this.state,
       events: updatedEvents
-    })
-
-  
+    })*/
+    this.writeUserData(updatedEvents)
+    
     }
+writeUserData=events=> {
+  firebase.database().ref('events/').set({
+   events
+  });
+}
 
+handleDelete=anEvent=> {
+    const updatedEvents = this.state.events.slice(0);
+    updatedEvents[anEvent.index].deleted = true;
+    this.writeUserData(updatedEvents)
+}
+
+componentWillMount(){
+    var readData = firebase.database().ref('events/');
+      readData.on('value',(snapshot) => {
+        if (snapshot.val() === null) {
+        this.setState({
+          ...this.state,
+          events: [],
+        })}
+        else {
+        this.setState({
+          ...this.state,
+          events: snapshot.val().events,
+        })}
+      })
+    } 
   render() {
     return (
       <div>
-      <form onSubmit={this.handleSubmit}>
+        
+      <form className = "submitter" onSubmit={this.handleSubmit}>
         <div classthing= "header">
         Event Tracker
         </div>
+        <ul>
         <label classthing = "info">
+          <li>
           Event:
-          <input type="text" value={this.state.thing} onChange={this.handleThingChange} />
+          <input type="text" value={this.state.thing} onChange={this.handleThingChange} /></li>
+          <li>
           Date:
-          <input type="text" value={this.state.date} onChange={this.handleDateChange} />
+          <input type="text" value={this.state.date} onChange={this.handleDateChange} /></li>
+          <li>
           Time:
-          <input type="text" value={this.state.time} onChange={this.handleTimeChange} />
+          <input type="text" value={this.state.time} onChange={this.handleTimeChange} /></li>
+          <li>
           Location:
-          <input type="text" value={this.state.location} onChange={this.handleLocationChange} />
+          <input type="text" value={this.state.location} onChange={this.handleLocationChange} /></li>
         </label>
-        <input type="submit" value="Submit" />
+        </ul>
+        <input className = "input" type="submit" value="Submit" />
       </form>
+      
       <ul>
       {this.state.events.map(anEvent => {
-   return <li> 
-     Event: {anEvent.thing} |
-     {anEvent.editing &&  <input type="text" value={this.state.temporary_thing} onChange={this.handleThingEdit} />} Date: {anEvent.date} |
-     {anEvent.editing &&  <input type="text" value={this.state.temporary_date} onChange={this.handleDateEdit} />} Time: {anEvent.time} |
-      {anEvent.editing &&  <input type="text" value={this.state.temporary_time} onChange={this.handleTimeEdit} />} Location: {anEvent.location} 
+   return <div> {!anEvent.deleted && <li className = "events"> 
+     Event: {!anEvent.editing && anEvent.thing+ ' |'} 
+     {anEvent.editing &&  <input type="text" value={this.state.temporary_thing} onChange={this.handleThingEdit} />} Date: {!anEvent.editing && anEvent.date + ' |'} 
+     {anEvent.editing &&  <input type="text" value={this.state.temporary_date} onChange={this.handleDateEdit} />} Time: {!anEvent.editing && anEvent.time + ' |'} 
+      {anEvent.editing &&  <input type="text" value={this.state.temporary_time} onChange={this.handleTimeEdit} />} Location: {!anEvent.editing && anEvent.location + ""} 
       {anEvent.editing &&  <input type="text" value={this.state.temporary_location} onChange={this.handleLocationEdit} />}
-       <button onClick = {() => {this.handleEdit(anEvent)}}> Edit</button > <button onClick = {() => {this.handleEditSubmit(anEvent)}}> Done </button></li>;
+       {!anEvent.editing && <button onClick = {() => {this.handleEdit(anEvent)}}> Edit</button >} 
+       {anEvent.editing && <button onClick = {() => {this.handleEditSubmit(anEvent)}}> Done </button>}
+       {!anEvent.editing && <button onClick = {() => {this.handleDelete(anEvent)}}> Delete</button >}
+       </li>}</div>;
 })}
      </ul>
       </div>
