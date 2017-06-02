dbdfc38d56da864fb6413115af94e021d3760b4b
Nojan Sheybani <nds4jp@virginia.edu>
2017-06-02 08:43:22 -0400

    starting instructors

--- app/src/components/Instructors.js
+++ app/src/components/Instructors.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import mike from './images/mike.jpg';
+import jack from './images/jackross.jpg';
+import brent from './images/brent.jpg'
+
 class Instructors extends Component {
   render() {
     return (
-      <div className="Motivation">
+      <div className="Instructors">
       Instructors
+      <img src={mike}></img>
       </div>
     );
   }
