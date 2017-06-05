12fc9fad970fe472ff4d1ae45fe6003ff27d9bf2
Nojan Sheybani <nds4jp@virginia.edu>
2017-06-05 09:02:26 -0400

    adding student page

--- app/src/components/Students.js
+++ app/src/components/Students.js
@@ -1,9 +1,86 @@
 import React, { Component } from 'react';
+import annie from './images/annie.jpg';
+import arpit from './images/arpit.jpg';
+import cameron from './images/cameron.jpg';
+import caroline from './images/caroline.jpg';
+import hung from './images/hung.jpg';
+import kavya from './images/kavya.jpg';
+import lindsey from './images/lindsey.jpg';
+import shavers from './images/shavers-lindsey.jpg';
+import melissa from './images/melissa.jpg';
+import mitch from './images/mitch.jpg';
+import nayanika from './images/nayanika.jpg';
+import nick from './images/nick.jpg';
+import nojan from './images/nojan.jpg';
+import paul from './images/paul.jpg';
+import sai from './images/sai.jpg';
+import spencer from './images/spencer.jpg';
+import taylor from './images/taylor.jpg';
+import vinny from './images/vinny.jpg';
+import will from './images/will.jpg';
+
 class Students extends Component {
   render() {
     return (
-      <div className="Instructors">
-     Students
+      <div className="Students">
+     <div className="annie">
+      <img className="img-circle" src={annie}></img>
+     </div>
+     <div className="arpit">
+      <img className="img-circle" src={arpit}></img>
+     </div>
+     <div className="cameron">
+      <img className="img-circle" src={cameron}></img>
+     </div>
+     <div className="caroline">
+      <img className="img-circle" src={caroline}></img>
+     </div>
+     <div className="hung">
+      <img className="img-circle" src={hung}></img>
+     </div>
+     <div className="kavya">
+      <img className="img-circle" src={kavya}></img>
+     </div>
+     <div className="lindsey">
+      <img className="img-circle" src={lindsey}></img>
+     </div>
+     <div className="shavers">
+      <img className="img-circle" src={shavers}></img>
+     </div>
+     <div className="melissa">
+      <img className="img-circle" src={melissa}></img>
+     </div>
+     <div className="mitch">
+      <img className="img-circle" src={mitch}></img>
+     </div>
+     <div className="nayanika">
+      <img className="img-circle" src={nayanika}></img>
+     </div>
+     <div className="nick">
+      <img className="img-circle" src={nick}></img>
+     </div>
+     <div className="nojan">
+      <img className="img-circle" src={nojan}></img>
+     </div>
+     <div className="paul">
+      <img className="img-circle" src={paul}></img>
+     </div>
+     <div className="sai">
+      <img className="img-circle" src={sai}></img>
+     </div>
+     <div className="spencer">
+      <img className="img-circle" src={spencer}></img>
+     </div>
+     <div className="taylor">
+      <img className="img-circle" src={taylor}></img>
+     </div>
+     <div className="vinny">
+      <img className="img-circle" src={vinny}></img>
+     </div>
+     <div className="will">
+      <img className="img-circle" src={will}></img>
+     </div>
+
       </div>
     );
   }
